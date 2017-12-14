47f3c5ef77f42cc328e19323cf9e9531aceaae82
TMineCola <s104213083@mail1.ncnu.edu.tw>
2017-12-14 14:31:00 +0800

    前後端分離

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var cors = require('cors');
 // var index = require('./routes/index');
 // var users = require('./routes/users');
 
@@ -13,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//  app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,9 +22,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/vendor', express.static(__dirname + '/public/vendor'));
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
