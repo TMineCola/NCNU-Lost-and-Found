3f31b27591b134fcc1c6f6842b4e4e26bb1da095
TMineCola <s104213083@mail1.ncnu.edu.tw>
2017-12-24 22:47:28 +0800

    修正路由錯誤時的訊息, 並新增開發模式選項(目前無用)

--- app.js
+++ app.js
@@ -84,11 +84,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = config.development === true ? err : {"message": "Page not found"};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(res.locals.error);
 });
 
 module.exports = app;
--- env.js
+++ env.js
@@ -1,6 +1,8 @@
 /* 用來設定Node.js 環境變數 */
 
 var config = {
+    // 開發者模式
+    development: false,
     // MySQL 帳號密碼
     SQL_HOST: "localhost",
     SQL_USER: "root",
