0b78182affac03a5304db70b91544db847888d0a
TMineCola <s104213083@mail1.ncnu.edu.tw>
2017-12-27 02:34:12 +0800

    新增env範例檔案

---
+++ env.example.js
@@ -0,0 +1,16 @@
+/* 用來設定Node.js 環境變數 */
+
+var config = {
+    // 開發者模式
+    development: false,
+    // MySQL 帳號密碼
+    SQL_HOST: "",
+    SQL_USER: "",
+    SQL_PWD: "",
+    SQL_DB: "",
+    // Facebook API
+    FB_ID: "",
+    FB_SECRET: ""
+};
+
+module.exports = config;
