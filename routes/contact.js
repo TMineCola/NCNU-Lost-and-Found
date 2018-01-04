faf285eff8958114271f4500d26ad5874a06c0e8
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-04 18:50:38 +0800

    註解更新

--- routes/contact.js
+++ routes/contact.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* 查詢全部拾獲物 */
+/* 查詢全部聯絡資訊 */
 function _Search(db) {
   return new Promise((resolve, reject) => {
     // 預設由新至舊
@@ -9,16 +9,16 @@ function _Search(db) {
     db.query(sql, function (err, result, fields) {
       if(err) {
         /* 查詢失敗時回傳訊息物件 */
-        reject({"message": "查詢全部拾獲物資訊失敗"});
+        reject({"message": "查詢全部聯絡資訊資訊失敗"});
       } else {
-        /* 新增成功時回傳拾獲物物件 */
+        /* 新增成功時回傳聯絡資訊物件 */
         resolve(result);
       }
     });
   });
 }
 
-/* 全部拾獲物 */
+/* 全部聯絡資訊 */
 router.get('/', function(req, res, next) {
   let db = req.dbstatus;
   _Search(db).then(foundObj => {
