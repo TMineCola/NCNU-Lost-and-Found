1cf4612048a591a4d4a50c8d27931c134ff742b5
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-06-29 12:11:26 +0800

    新增刪除修改完成

--- routes/account.js
+++ routes/account.js
@@ -6,14 +6,34 @@ var db = require('./modules/accountController');
 
 router.post('/', function(req, res, next) {
     let account = req.body;
+    db.searchEmail(account.email).then(() => {
+        return db.create(account.email, account.password);
+    }).then((insertID) => {
+        return res.status(200).send({ "insertID": insertID });
+    }).catch(() => {
+        return res.status(500).send({ "error": "insertError" });
+    })
 });
 
 router.patch('/', function(req, res, next) {
-
+    let account = req.body;
+    db.searchEmail(account.email).then(() => {
+        return db.modify(account.newemail, account.password, account.oldemail);
+    }).then(() => {
+        return res.status(200).send({ "success": "modifySuccess" });
+    }).catch(() => {
+        return res.status(500).send({ "error": "modifyError" });
+    });
 });
 
 router.delete('/', function(req, res, next) {
-
+    db.searchEmail(account.email).then(() => {
+        return db.modify(account.email, account.password);
+    }).then(() => {
+        return res.status(200).send({ "success": "deleteSuccess" });
+    }).catch(() => {
+        return res.status(500).send({ "error": "deleteError" });
+    });
 });
 
 module.exports = router;
