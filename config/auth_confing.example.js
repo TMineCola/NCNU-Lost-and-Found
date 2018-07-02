4d624def9ffbe33a4a50271644b54a030ddbab96
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-07-02 17:48:01 +0800

    消失的example回歸

---
+++ config/auth_confing.example.js
@@ -0,0 +1,17 @@
+/* 設定Passport auth */
+
+var auth_config = {
+    'facebookAuth' :{
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
+    },
+
+    'googleAuth' :{
+        'clientID'      : '',
+        'clientSecret'  : '',
+        'callbackURL'   : ''
+    }
+};
+
+module.exports = auth_config;
