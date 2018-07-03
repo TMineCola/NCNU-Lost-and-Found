1725f025d852814bde3d5116ba44e37d9499b395
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-07-03 21:42:13 +0800

    砍掉87 Lost 修改狀態

--- routes/admin.js
+++ routes/admin.js
@@ -22,23 +22,4 @@ router.post('/changeState/found', function(req, res, next) {
     Promise.all(runtime).then(() => res.status(200).send(response)).catch(error => res.status(500).send(error));
 });
 
-router.post('/changeState/lost', function(req, res, next) {
-    let object = req.body;
-    if(typeof(object['state']) != "number" || object['state'] < 0 || object['state'] > 7) {
-        res.status(500).send({"failed": "非正確狀態"});
-    }
-    let response = {
-        failed: []
-    };
-    let runtime = [];
-
-    object["ID"].forEach(ID => {
-        runtime.push(db.changeState('property_lostwish', ID, object.state).catch(() => {
-            response.failed.push(ID);
-        }));
-    })
-
-    Promise.all(runtime).then(() => res.status(200).send(response)).catch(error => res.status(500).send(error));
-});
-
 module.exports = router;
