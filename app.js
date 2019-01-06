04249137df9431b3c31ac7ee01b5eca4fe0ed433
TMineCola <s104213083@mail1.ncnu.edu.tw>
2019-01-06 17:21:11 +0800

    added csrf

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/vendor', express.static(__dirname + '/public/vendor'));
+app.use(csurf({ cookie: true }));
 
 // 將資料庫連線狀態帶入
 app.use(function (req, res, next) {
@@ -73,40 +74,40 @@ app.use(function (req, res, next) {
 
 // 網頁路由
 app.get('/', function (req, res) {
-    res.render('pages/index', { user: req.user, page_name: 'index' });
+    res.render('pages/index', { user: req.user, page_name: 'index', csrfToken: req.csrfToken()});
 });
 app.get('/found', function (req, res) {
-    res.render('pages/found', { user: req.user, page_name: 'found' });
+    res.render('pages/found', { user: req.user, page_name: 'found', csrfToken: req.csrfToken()});
 });
 app.get('/lost', function (req, res) {
-    res.render('pages/lost', { user: req.user, page_name: 'lost' });
+    res.render('pages/lost', { user: req.user, page_name: 'lost', csrfToken: req.csrfToken()});
 });
 app.get('/expired', function (req, res) {
-    res.render('pages/expired', { user: req.user, page_name: 'expired' });
+    res.render('pages/expired', { user: req.user, page_name: 'expired', csrfToken: req.csrfToken()});
 });
 app.get('/claim', function (req, res) {
-    res.render('pages/claim', { user: req.user, page_name: 'claim' });
+    res.render('pages/claim', { user: req.user, page_name: 'claim', csrfToken: req.csrfToken()});
 });
 app.get('/contact', function (req, res) {
-    res.render('pages/contact', { user: req.user, page_name: 'contact' });
+    res.render('pages/contact', { user: req.user, page_name: 'contact', csrfToken: req.csrfToken()});
 });
 app.get('/editor_found', function (req, res) {
-    res.render('pages/editor_found', { user: req.user, page_name: 'editor_found' });
+    res.render('pages/editor_found', { user: req.user, page_name: 'editor_found', csrfToken: req.csrfToken()});
 });
 app.get('/editor_lost', function (req, res) {
-    res.render('pages/editor_lost', { user: req.user, page_name: 'editor_lost' });
+    res.render('pages/editor_lost', { user: req.user, page_name: 'editor_lost', csrfToken: req.csrfToken()});
 });
 app.get('/editor_found-admin', function (req, res) {
-    res.render('pages/editor_found-admin', { user: req.user, page_name: 'editor_found-admin' });
+    res.render('pages/editor_found-admin', { user: req.user, page_name: 'editor_found-admin', csrfToken: req.csrfToken()});
 });
 app.get('/editor_lost-admin', function (req, res) {
-    res.render('pages/editor_lost-admin', { user: req.user, page_name: 'editor_lost-admin' });
+    res.render('pages/editor_lost-admin', { user: req.user, page_name: 'editor_lost-admin', csrfToken: req.csrfToken()});
 });
 app.get('/wishlist_form', function (req, res) {
-    res.render('pages/wishlist_form', { user: req.user, page_name: 'wishlish_form' });
+    res.render('pages/wishlist_form', { user: req.user, page_name: 'wishlish_form', csrfToken: req.csrfToken()});
 });
 app.get('/lost_form', function (req, res) {
-    res.render('pages/lost_form', { user: req.user, page_name: 'lost_form' });
+    res.render('pages/lost_form', { user: req.user, page_name: 'lost_form', csrfToken: req.csrfToken()});
 });
 
 // 驗證路由
