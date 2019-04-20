c2fb22848846a4c8e184917d7d54a10ba501653f
TMineCola <s104213083@mail1.ncnu.edu.tw>
2019-04-20 16:30:18 +0800

    add changepassword and check data

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,3 @@
-var sha256 = require('sha256');
 var express = require('express');
 var router = express.Router();
 var middleware = require('./middleware/login');
@@ -6,6 +5,10 @@ var db = require('./modules/accountController');
 
 router.get('/', function(req, res, next) {
     let account = req.body;
+    if(!account || !account.email) {
+        res.status(406).send({ "error": "should provide email" });
+        return;
+    }
     db.searchAccount(account.email).then((status) => {
         if(status) {
             res.status(403).send({ "error": "already enroll"});
@@ -22,6 +25,10 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     let account = req.body;
+    if(!account || !account.email || !account.password) {
+        res.status(406).send({ "error": "should provide email and password" });
+        return;
+    }
     db.searchAccount(account.email).then((status) => {
         if(status) {
             res.status(403).send({ "error": "already enroll"});
@@ -38,6 +45,10 @@ router.post('/', function(req, res, next) {
 
 router.patch('/', function(req, res, next) {
     let account = req.body;
+    if(!account || !account.email || !account.newemail || !account.password) {
+        res.status(406).send({ "error": "should provide email, newemail and password" });
+        return;
+    }
     db.searchAccount(account.email).then((status) => {
         if(status) {
             return db.verifyAccount(account.email, account.password);
@@ -54,8 +65,34 @@ router.patch('/', function(req, res, next) {
     });
 });
 
+router.patch('/password', function(req, res, next) {
+    let account = req.body;
+    if(!account || !account.email || !account.password || !account.newpassword) {
+        res.status(406).send({ "error": "should provide email, password and newpassword" });
+        return;
+    }
+    db.searchAccount(account.email).then((status) => {
+        if(status) {
+            return db.changePassword(account.email, account.password, account.newpassword);
+        } else {
+            res.status(403).sned({ "error": "not enroll"});
+            return;
+        }
+    }).then(() => {
+        return db.modify(account.newemail, account.password, account.email);
+    }).then(() => {
+        return res.status(200).send({ "success": "change password success" });
+    }).catch(() => {
+        return res.status(500).send({ "error": "change password error" });
+    });
+});
+
 router.delete('/', function(req, res, next) {
     let account = req.body;
+    if(!account || !account.email || !account.password) {
+        res.status(406).send({ "error": "should provide email and password" });
+        return;
+    }
     db.searchAccount(account.email).then((status) => {
         if(status) {
             return db.verifyAccount(account.email, account.password);
